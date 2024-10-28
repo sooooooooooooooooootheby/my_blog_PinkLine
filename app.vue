0e88bccfa10d07b4f3a168291b086a23985e87c7
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2024-10-28 15:19:49 +0800

    小修样式 删了一个测试文件

--- app.vue
+++ app.vue
@@ -26,33 +26,26 @@ useHead({
 
 <style lang="scss" scoped>
 header {
-	width: calc(90vw - 40vw);
-	height: 83px;
-	padding: 0 25vw;
-	position: fixed;
-	top: 0;
-	left: 0;
-	z-index: 2;
-	backdrop-filter: blur(20px);
+	width: 700px;
+	height: auto;
+	margin: 0 auto;
+	padding-top: 82px;
 }
 main {
-	width: 800px;
+	width: 700px;
 	margin: 0 auto;
-	padding-top: 83px;
 	position: relative;
 	z-index: 1;
 }
 footer {
-	width: 50vw;
+	width: 700px;
 	margin: 0 auto;
 	padding: 20px 0;
-	position: relative;
-	z-index: 1;
 }
 
 @media (max-width: 900px) {
 	header {
-		width: calc(100vw - 5vw);
+		width: calc(100vw - 40px);
 		height: 48px;
 		padding: 0 2.5vw;
 	}
