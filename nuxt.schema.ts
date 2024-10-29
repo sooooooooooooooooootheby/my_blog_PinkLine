2447783b28fe710a9021b6336e1f57da0c090176
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2024-10-29 16:03:39 +0800

    nuxt/studio测试

--- nuxt.schema.ts
+++ nuxt.schema.ts
@@ -2,18 +2,30 @@ import { field, group } from "@nuxthq/studio/theme";
 
 export default defineNuxtSchema({
 	appConfig: {
-		parent: group({
-			title: "UI",
-			description: "UI configuration",
-			icon: "i-ph-palette-fill",
+		// parent: group({
+		// 	title: "UI",
+		// 	description: "UI configuration",
+		// 	icon: "i-ph-palette-fill",
+		// 	fields: {
+		// 		primary: field({
+		// 			type: "string",
+		// 			title: "Primary",
+		// 			description: "Primary color of your UI.",
+		// 			icon: "i-ph-palette",
+		// 			default: "sky",
+		// 			required: ["sky", "mint", "rose", "amber"],
+		// 		}),
+		// 	},
+		// }),
+		index: group({
+			title: "首页",
+			description: "首页的配置信息",
 			fields: {
-				primary: field({
-					type: "string",
-					title: "Primary",
-					description: "Primary color of your UI.",
-					icon: "i-ph-palette",
-					default: "sky",
-					required: ["sky", "mint", "rose", "amber"],
+				skill: field({
+					type: "array",
+					title: "技术",
+					description: "这里写你会的技术",
+					default: ["JavaScript", "Vue.js", "Node.js", "C", "Kotlin", "MySql"],
 				}),
 			},
 		}),
