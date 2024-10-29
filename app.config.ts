447303f522c1bb24ae8b0b6f2bcd563b40fb7cc1
Sooooooooooooooooootheby <120046369+sooooooooooooooooootheby@users.noreply.github.com>
2024-10-29 16:46:10 +0800

    1 change by Nuxt Studio

--- app.config.ts
+++ app.config.ts
@@ -17,6 +17,30 @@ export default defineAppConfig({
       title: "Discord 表情贴纸下载器",
       description: "这是一个可以下载discord服务器中表情和贴纸的下载工具",
       url: "https:dge.sooooooooooooooooootheby.top/"
+    }],
+
+    api: [{
+      title: "网易云音乐api",
+      description: "这个是大佬写的非常强大的api服务, 可惜被律师函警告了, fork了一份自己建服务",
+      url: "https://netease.sooooooooooooooooootheby.top/NeteaseCloudMusicApi/"
+    }, {
+      title: "网易云无损解析",
+      description: "这个是另外一个大佬写的api, 可以解析下载音乐, 很强, 我自己建了服务用来改进了她的解析功能.",
+      url: "https://netease.sooooooooooooooooootheby.top/Netease_url/"
+    }],
+
+    friend: [{
+      name: "Sooooooooooooooooootheby",
+      url: "#"
+    }, {
+      name: "lassksy",
+      url: "https://www.woodude.top/"
+    }, {
+      name: "BLxcwg666",
+      url: "https://blog.nekorua.com/"
+    }, {
+      name: "Imken",
+      url: "https://imken.moe"
     }]
   }
 });
