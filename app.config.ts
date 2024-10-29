ab1d37c3291baa0cebd1da1a8d41a7af4fdd5043
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2024-10-29 14:57:38 +0800

    nuxt/studio测试

--- app.config.ts
+++ app.config.ts
@@ -1,20 +1,6 @@
 import { field, group } from "@nuxthq/studio/theme";
 
 export default defineAppConfig({
-	parent: group({
-		title: "Parent title",
-		description: "Parent description.",
-		icon: "i-icon-to-display",
-		fields: {
-			leaf: field({
-				type: "string",
-				title: "Field title",
-				description: "Field Description",
-				icon: "i-icon-to-display",
-				default: "default value",
-			}),
-		},
-	}),
 	skill: ["JavaScript", "Vue.js", "Node.js", "C", "Kotlin", "MySql"],
 	project: [
 		{
