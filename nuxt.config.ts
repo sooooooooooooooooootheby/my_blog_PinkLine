a0d3a1aea1aa8389ef676d4508806c5b1358e83a
Sooooooooooooooooootheby <120046369+sooooooooooooooooootheby@users.noreply.github.com>
2024-11-01 08:14:43 +0800

    更新 nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
 	compatibilityDate: "2024-04-03",
 	devtools: { enabled: true },
 
-	modules: ["@nuxt/icon", "@nuxt/content"],
+	modules: ["@nuxt/icon", "@nuxt/content", "@nuxthq/studio"],
 	content: {
 		highlight: {
 			theme: "github-dark",
