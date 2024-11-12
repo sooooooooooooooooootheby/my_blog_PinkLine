123214ecd89f17de0b6bd6bf924606674b296aa9
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2024-11-12 14:39:22 +0800

    文章列表按时间排序

--- pages/article.vue
+++ pages/article.vue
@@ -1,51 +1,51 @@
 <template>
-	<div class="articleList">
-		<ContentList path="/articles" v-slot="{ list }">
-			<ul>
-				<li class="articleBox" v-for="article in list" :key="article._path">
-					<div class="info">
-						<p class="time">{{ handleTime(article.data) }}</p>
-						<NuxtLink :to="`${article._path}`">
-							<div>
-								<p class="title">{{ article.title }}</p>
-							</div>
-						</NuxtLink>
-					</div>
-					<p class="categories">{ {{ article.categories }} }</p>
-					<p class="description">{{ article.description }}</p>
-				</li>
-			</ul>
-		</ContentList>
-	</div>
+    <div class="articleList">
+        <div class="article" v-for="item in articles" :key="item._path">
+            <a :href="item._path">
+                <div class="head">
+                    <div class="title">{{ item.title }}</div>
+                    <div class="time">
+                        {{ handleTime(item.data) }}
+                    </div>
+                </div>
+                <div class="description">{{ item.description }}</div>
+            </a>
+        </div>
+    </div>
 </template>
 
 <script setup>
 useHead({
-	title: "article",
+    title: "article",
 });
+
+const articles = await queryContent("articles")
+    .where({ _path: { $contains: "/articles/" } })
+    .sort({ data: -1 })
+    .find();
+
 const handleTime = (time) => {
-	if (time === null) {
-		this.updateTime = false;
-		return;
-	}
+    if (time === null || time === undefined) {
+        return;
+    }
 
-	// 使用Date对象解析ISO 8601格式的时间戳
-	const date = new Date(time);
+    // 使用Date对象解析ISO 8601格式的时间戳
+    const date = new Date(time);
 
-	// 解析时间
-	const months = date.getMonth();
-	const day = date.getDate();
-	const year = date.getFullYear();
-	const hours = date.getHours().toString().padStart(2, "0"); // 确保两位数
-	const minutes = date.getMinutes().toString().padStart(2, "0"); // 确保两位数
-	const seconds = date.getSeconds().toString().padStart(2, "0"); // 确保两位数
+    // 解析时间
+    const months = date.getMonth();
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const hours = date.getHours().toString().padStart(2, "0"); // 确保两位数
+    const minutes = date.getMinutes().toString().padStart(2, "0"); // 确保两位数
+    const seconds = date.getSeconds().toString().padStart(2, "0"); // 确保两位数
 
-	// 将月份转换为缩写格式
-	const monthArray = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-	const month = monthArray[months];
+    // 将月份转换为缩写格式
+    const monthArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+    const month = monthArray[months];
 
-	// 返回格式化的时间字符串
-	return `${month} ${day}, ${year}`;
+    // 返回格式化的时间字符串
+    return `${year} ${month}-${day}`;
 };
 </script>
 
