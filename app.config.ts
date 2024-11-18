1e16bcde2f4cb6b382bd74a831f28839328ade0f
Sooooooooooooooooootheby <120046369+sooooooooooooooooootheby@users.noreply.github.com>
2024-11-18 10:28:50 +0800

    1 change by Nuxt Studio

--- app.config.ts
+++ app.config.ts
@@ -12,11 +12,11 @@ export default defineAppConfig({
     project: [{
       title: "网易云无损解析",
       description: "如果你苦于没有黑胶会员或者想要转战本地播放器, 不妨试试这个网站, 它可以下载无损音乐",
-      url: "https:neteasepage.sooooooooooooooooootheby.top/"
+      url: "https://neteasepage.sooooooooooooooooootheby.top/"
     }, {
       title: "Discord 表情贴纸下载器",
       description: "这是一个可以下载discord服务器中表情和贴纸的下载工具",
-      url: "https:dge.sooooooooooooooooootheby.top/"
+      url: "https://dge.sooooooooooooooooootheby.top/"
     }],
 
     api: [{
