6e7f7b786f5ad653b29877d8df0396c359d5cab0
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2024-11-25 16:30:58 +0800

    删掉了一些多余的代码

--- plugins/oneko/oneko.js
+++ plugins/oneko/oneko.js
@@ -1,4 +1,5 @@
 /**
+ * oneko.js: https://github.com/adryd325/oneko.js
  *
  * 原作者: adryd325: https://github.com/adryd325
  * 修改: s22y: https://github.com/sooooooooooooooooootheby
@@ -29,68 +30,12 @@ export default defineNuxtPlugin((nuxtApp) => {
         let mousePosX = 0;
         let mousePosY = 0;
 
-        const nekoSites = ["localhost"];
-
-        try {
-            const searchParams = location.search
-                .replace("?", "")
-                .split("&")
-                .map((keyvaluepair) => keyvaluepair.split("="));
-            tmp = searchParams.find((a) => a[0] == "catx");
-            if (tmp && tmp[1]) nekoPosX = parseInt(tmp[1]);
-            tmp = searchParams.find((a) => a[0] == "caty");
-            if (tmp && tmp[1]) nekoPosY = parseInt(tmp[1]);
-            tmp = searchParams.find((a) => a[0] == "catdx");
-            if (tmp && tmp[1]) mousePosX = parseInt(tmp[1]);
-            tmp = searchParams.find((a) => a[0] == "catdy");
-            if (tmp && tmp[1]) mousePosY = parseInt(tmp[1]);
-        } catch (e) {
-            console.error("oneko.js: failed to parse query params.");
-            console.error(e);
-        }
-
-        function onClick(event) {
-            let target;
-            if (event.target.tagName === "A" && event.target.getAttribute("href")) {
-                target = event.target;
-            } else if (
-                event.target.tagName == "IMG" &&
-                event.target.parentElement.tagName === "A" &&
-                event.target.parentElement.getAttribute("href")
-            ) {
-                target = event.target.parentElement;
-            } else {
-                return;
-            }
-            let newLocation;
-            try {
-                newLocation = new URL(target.href);
-            } catch (e) {
-                return;
-            }
-            if ((nekoSites.includes(newLocation.host) && newLocation.pathname == "/") || target.dataset.neko) {
-                newLocation.searchParams.append("catx", Math.floor(nekoPosX));
-                newLocation.searchParams.append("caty", Math.floor(nekoPosY));
-                newLocation.searchParams.append("catdx", Math.floor(mousePosX));
-                newLocation.searchParams.append("catdy", Math.floor(mousePosY));
-                event.preventDefault();
-                window.location.href = newLocation.toString();
-            }
-        }
-        document.addEventListener("click", onClick);
-
-        // 每当动画播放一帧时，这个计数器就会增加。这个变量通常用于控制动画的速度、循环播放等。
         let frameCount = 0;
-        // 这个变量通常用于检测用户是否长时间没有与页面交互，从而触发相应的空闲动画或行为。
         let idleTime = 0;
-        // 这个变量可以是任何类型的对象，比如一个动画实例或一个函数，用于在用户长时间不活动时播放。
         let idleAnimation = null;
-        // 这个变量与frameCount类似，但专门用于跟踪空闲动画的播放进度。
         let idleAnimationFrame = 0;
 
-        // neko 速度
         const nekoSpeed = 10;
-        // 截取 neko 在图片中的位置
         const spriteSets = {
             idle: [[-3, -3]],
             alert: [[-7, -3]],
@@ -154,7 +99,6 @@ export default defineNuxtPlugin((nuxtApp) => {
             ],
         };
 
-        // neko 初始化
         function init() {
             nekoEl.id = "oneko";
             nekoEl.ariaHidden = true;
@@ -165,10 +109,8 @@ export default defineNuxtPlugin((nuxtApp) => {
             nekoEl.style.imageRendering = "pixelated";
             nekoEl.style.left = `${nekoPosX - 16}px`;
             nekoEl.style.top = `${nekoPosY - 16}px`;
-            nekoEl.style.zIndex = Number.MAX_VALUE;
-            nekoEl.style.zIndex = 9999;
+            nekoEl.style.zIndex = 2147483647;
 
-            // let nekoFile = "~/plugins/oneko/oneko.gif";
             const curScript = document.currentScript;
             if (curScript && curScript.dataset.cat) {
                 nekoFile = curScript.dataset.cat;
@@ -185,8 +127,8 @@ export default defineNuxtPlugin((nuxtApp) => {
             window.requestAnimationFrame(onAnimationFrame);
         }
 
-        // 创建动画循环
         let lastFrameTimestamp;
+
         function onAnimationFrame(timestamp) {
             if (!nekoEl.isConnected) {
                 return;
@@ -198,25 +140,19 @@ export default defineNuxtPlugin((nuxtApp) => {
                 lastFrameTimestamp = timestamp;
                 frame();
             }
-
             window.requestAnimationFrame(onAnimationFrame);
         }
 
-        // 根据帧数来更新 neko 的显示图像
         function setSprite(name, frame) {
-            // 移动 *
             const sprite = spriteSets[name][frame % spriteSets[name].length];
-            // 动画切换 *
             nekoEl.style.backgroundPosition = `${sprite[0] * 32}px ${sprite[1] * 32}px`;
         }
 
-        // neko 停止时调用
         function resetIdleAnimation() {
             idleAnimation = null;
             idleAnimationFrame = 0;
         }
 
-        // 闲置
         function idle() {
             idleTime += 1;
 
@@ -265,7 +201,6 @@ export default defineNuxtPlugin((nuxtApp) => {
             idleAnimationFrame += 1;
         }
 
-        // 控制 neko 的动画
         function frame() {
             frameCount += 1;
             const diffX = nekoPosX - mousePosX;
