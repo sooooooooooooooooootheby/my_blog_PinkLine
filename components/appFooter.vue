9a2878e4318407be13f4d9f083af3fd3eaeae6ef
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2024-12-09 16:02:54 +0800

    优化页脚布局

--- components/appFooter.vue
+++ components/appFooter.vue
@@ -1,20 +1,18 @@
 <template>
     <div class="appFooter">
-        <div class="up">
-            <ul class="menu">
-                <li class="item">
-                    <NuxtLink to="https://www.travellings.cn/go.html" target="_blank"> TRAVELLING </NuxtLink>
-                </li>
-                <li class="item">
-                    <NuxtLink to="/rss.xml" target="_blank"> RSS </NuxtLink>
-                </li>
-                <li class="item">
-                    <NuxtLink to="/exercise">EXERCISE</NuxtLink>
-                </li>
-            </ul>
-            <div class="info">
-                <p class="copyright">Copyright ©2024.sooooooooooooooooootheby</p>
-            </div>
+        <ul class="menu">
+            <li class="item">
+                <NuxtLink to="https://www.travellings.cn/go.html" target="_blank"> TRAVELLING </NuxtLink>
+            </li>
+            <li class="item">
+                <NuxtLink to="/rss.xml" target="_blank"> RSS </NuxtLink>
+            </li>
+            <li class="item">
+                <NuxtLink to="/exercise">EXERCISE</NuxtLink>
+            </li>
+        </ul>
+        <div class="info">
+            <p class="copyright">Copyright ©2024.sooooooooooooooooootheby</p>
         </div>
     </div>
 </template>
