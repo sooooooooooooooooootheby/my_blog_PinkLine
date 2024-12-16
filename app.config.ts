dedfc8112d50fd32c9ee4e0b4299d8c155e75493
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2024-12-16 13:47:04 +0800

    新增友链

--- app.config.ts
+++ app.config.ts
@@ -61,6 +61,11 @@ export default defineAppConfig({
       name: "ZLA 小站",
       url: "https://www.zla.pub/",
       button: "/friend/zla.webp"
+    },
+    {
+      name: "Pinpe 的云端",
+      url: "https://pinpe.top",
+      button: "https://pinpe.top/head.jpg"
     }]
   },
 
