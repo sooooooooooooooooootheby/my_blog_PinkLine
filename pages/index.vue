3c294551681254066ee4c1ee3bb71a2fb1af55f7
Sooooooooooooooooootheby <120046369+sooooooooooooooooootheby@users.noreply.github.com>
2024-12-18 20:50:29 +0800

    更新 index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -5,15 +5,15 @@
                 <p class="greet_title1">Hi.👋</p>
                 <p class="greet_title2">I'm</p>
                 <p class="greet_title3"><Icon class="icon" name="icon-park-solid:connect-address-two" />现居 中国南宁.</p>
-                <p class="greet_title3"><Icon class="icon" name="icon-park-solid:blossom" />梦想是躺平什么都不用做.</p>
+                <p class="greet_title3"><Icon class="icon" name="icon-park-solid:blossom" /><del>梦想是躺平什么都不用做.</del>已经是个尸体了</p>
             </div>
             <div class="introduce">
-                <p class="introduce_1">大学在读, 专业是计算机网络技术.</p>
+                <p class="introduce_1">在读, 专业是计算机网络技术.</p>
                 <p class="introduce_2">
-                    目前在备战专升本考试, 业余时间会做一些小玩意, 可以去我的<a
+                    目前在备战专升本考试, 业余时间会做一些没用的小玩意, 可以去我的<a
                         href="https://github.com/sooooooooooooooooootheby?tab=repositories"
                         >Github</a
-                    >查看.
+                    >看看. 如果能够帮到你我会很开心.
                 </p>
             </div>
             <img :src="randomImage()" alt="headerBar" class="headerBar" />
