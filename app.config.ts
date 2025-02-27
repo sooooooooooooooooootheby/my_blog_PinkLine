f7cd3abf5d1da29a2df471868797e7048da419a1
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2025-02-27 17:51:11 +0800

    add api

--- app.config.ts
+++ app.config.ts
@@ -3,15 +3,15 @@ export default defineAppConfig({
     project: [{
       title: "网易云无损解析",
       description: "如果你苦于没有黑胶会员或者想要转战本地播放器, 不妨试试这个网站, 它可以下载无损音乐",
-      url: "https://music.sooooooooooooooooootheby.top/"
+      url: "https://music.s22y.moe/"
     }, {
       title: "Discord 表情贴纸下载器",
       description: "这是一个可以下载discord服务器中表情和贴纸的下载工具",
-      url: "https://dge.sooooooooooooooooootheby.top/"
+      url: "https://dge.s22y.moe/"
     }, {
       title: "服务器监控面板",
       description: "这是一个服务器性能监测站点",
-      url: "https://server.sooooooooooooooooootheby.top/"
+      url: "https://server.s22y.moe/"
     }, {
       title: "flex 试验场",
       description: "这是学 css 时老师给的试验场, 之前 gitee 也有一份, 还有在线站点, 不过已经下线好久了, 自己搭了一个, 虽然学了很久了, 但是时不时还是会用到.",
@@ -30,6 +30,10 @@ export default defineAppConfig({
       title: "网易云无损解析",
       description: "这个是另外一个大佬写的api, 可以解析下载音乐, 很强, 我自己建了服务用来改进了她的解析功能.",
       url: "https://api.sooooooooooooooooootheby.top/Netease_url/"
+    }, {
+      title: "计数器",
+      description: "这是一个计数器, 可以用来计数, eg: https://api.s22y.moe/count/get?name=Alice",
+      url: "https://api.s22y.moe/count/get?name=Alice",
     }],
 
     friend: [{
