de4b29b24daafe9de2206a66300ec678c43b98e3
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2025-04-01 23:42:32 +0800

    更新 评论区的地址以及表情地址

--- components/waline.vue
+++ components/waline.vue
@@ -8,11 +8,11 @@ import { init } from "@waline/client";
 onMounted(() => {
     init({
         el: "#waline",
-        serverURL: "https://comment.s22y.moe/",
+        serverURL: "https://comment.blog.s22y.moe/",
         reaction: false,
         search: false,
         dark: "html[class='dark-mode']",
-        emoji: ["https://gcore.jsdelivr.net/gh/sooooooooooooooooootheby/pinkline_commit@v1.0.0/r1999"],
+        emoji: ["https://gcore.jsdelivr.net/gh/sooooooooooooooooootheby/emoji@b1.0.1/r1999"],
     });
 });
 </script>
